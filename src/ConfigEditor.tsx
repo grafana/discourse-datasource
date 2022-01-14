adb6eca2e840e5997a5dfec065db216c99cd3659
Matt Abrams <37156449+zuchka@users.noreply.github.com>
2022-01-14 09:14:05 -1000

    switch configeditor from classes to hooks

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -1,25 +1,34 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { DataSourceHttpSettings } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { DiscourseDataSourceOptions, DiscourseSecureJsonData } from './types';
 
 interface Props extends DataSourcePluginOptionsEditorProps<DiscourseDataSourceOptions, DiscourseSecureJsonData> {}
 
-interface State {}
+export const ConfigEditor: React.FC<Props> = ({ options, onOptionsChange }) => {
+  return (
+    <>
+      <DataSourceHttpSettings
+        defaultUrl="http://localhost:16686"
+        dataSourceConfig={options}
+        showAccessOptions={false}
+        onChange={onOptionsChange}
+      />
+    </>
+  )
+// export class ConfigEditor extends PureComponent<Props, State> {
+//   render() {
+//     const { options, onOptionsChange } = this.props;
 
-export class ConfigEditor extends PureComponent<Props, State> {
-  render() {
-    const { options, onOptionsChange } = this.props;
-
-    return (
-      <>
-        <DataSourceHttpSettings
-          defaultUrl="http://localhost:9090"
-          dataSourceConfig={options}
-          showAccessOptions={true}
-          onChange={onOptionsChange}
-        />
-      </>
-    );
-  }
+//     return (
+//       <>
+//         <DataSourceHttpSettings
+//           defaultUrl="http://localhost:9090"
+//           dataSourceConfig={options}
+//           showAccessOptions={true}
+//           onChange={onOptionsChange}
+//         />
+//       </>
+//     );
+//   }
 }
